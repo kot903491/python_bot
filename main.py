7b915e523fdfbbb74e6c4435a5b887fe92a95cbe
Timurka <kot903491@gmail.ru>
2023-02-17 00:33:45 +0500

    aiogram

--- main.py
+++ main.py
@@ -1,65 +1,52 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 import requests
-import time
-import match
 
-API_URL:str = 'https://api.telegram.org/bot'
-BOT_TOKEN:str = '5849642826:AAFOvnJTbn1sDvVO7cT4HcV1K8Lxo08cIk8'
-TEXT:str = 'Ура! Классный апдейт!'
-MAX_COUNTER: int = 30
-
-API_CATS_URL: str = 'https://aws.random.cat/meow'
-ERROR_TEXT:str ='Здесь должна быть картинка :('
-
-
-offset: int = -2
-counter: int = 0
-chat_id: int
-
-api_photo:str
 api_response = requests.Response
-text:str
-photo_link:str
+ERROR_TEXT = 'Тут должна быть картинка :('
+
+API_TOKEN:str = '5849642826:AAFOvnJTbn1sDvVO7cT4HcV1K8Lxo08cIk8'
+bot:Bot = Bot(token=API_TOKEN)
+dp:Dispatcher=Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message:Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+    
+@dp.message(Command(commands=["ahtung"]))
+async def process_ahtyng_command(message:Message):
+    api_photo = 'https://random.dog/woof.json'
+    api_response=requests.get(api_photo)
+    if api_response.status_code==200:
+        photo_link = api_response.json()['url']
+        await message.answer_photo(photo_link)
+    else:
+        await message.answer(ERROR_TEXT)
+        
+@dp.message(Command(commands=["valera"]))
+async def process_valera_command(message:Message):
+    api_photo = 'https://randomfox.ca/floof/'
+    api_response=requests.get(api_photo)
+    if api_response.status_code==200:
+        photo_link = api_response.json()['image']
+        await message.answer_photo(photo_link, caption='Вот такой Валера!')
+    else:
+        await message.answer(ERROR_TEXT)
 
 
-while counter < MAX_COUNTER:
-    b:bool=False
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            text = result['message']['text']
-            
-            match text:
-                case "/help":
-                    api_photo = 'https://aws.random.cat/meow'
-                    api_response = requests.get(api_photo)   
-                    if api_response.status_code==200:
-                        photo_link = api_response.json()['file']
-                        b=True
-                case "/ahtung":
-                    api_photo = 'https://random.dog/woof.json'
-                    api_response = requests.get(api_photo)   
-                    if api_response.status_code==200:
-                        photo_link = api_response.json()['url']
-                        b=True
-                case "/valera":
-                    api_photo = 'https://randomfox.ca/floof/'
-                    api_response = requests.get(api_photo)   
-                    if api_response.status_code==200:
-                        photo_link = api_response.json()['image']
-                        b=True
-                case _:
-                    pass
-                    
-            if b:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={photo_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-                            
-                                
-            
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
 
-    time.sleep(1)
-    counter += 1
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
