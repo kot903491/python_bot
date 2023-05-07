1874b78b43f065b6319bb661edb58d018ae959cd
Timurka <kot903491@gmail.ru>
2023-05-07 22:36:20 +0500

    конфиг

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from config_data.config import load_config
 
-
 config = load_config('.env')
 print(config.tg_bot.token)
--- config_data/config.py
+++ config_data/config.py
@@ -13,21 +13,16 @@ class DatabaseConfig:
 @dataclass()
 class TgBot:
     token: str  # Токен к боту
-    admin_ids: list[int]  # Список админов бота
+    # admin_ids: list[int]  # Список админов бота
 
 
 @dataclass()
 class Config:
     tg_bot: TgBot
-    db: DatabaseConfig
+    # db: DatabaseConfig
 
 
 def load_config(path: str | None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
-                               admin_ids=list(map(int, env.list('ADMIN_IDS')))),
-                  db=DatabaseConfig(db_name=env('DATABASE'),
-                                    db_host=env('DB_HOST'),
-                                    db_user=env('DB_USER'),
-                                    db_password=env('DB_PASSWORD')))
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
